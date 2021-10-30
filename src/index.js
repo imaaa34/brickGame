67e6aa3d6b04948fbc83cc129916ba60c6e37ef1
Akari Imade <81577119+imaaa34@users.noreply.github.com>
2021-10-30 12:15:57 +0900

    [Add] paddle move

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ let ctx = canvas.getContext("2d"); //2dで平面図形を描く
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, 800, 600);
-
 // ctx.fillStyle = '#f00';
 // ctx.fillRect(30, 10, 100, 100);
 
@@ -19,3 +17,17 @@ ctx.clearRect(0, 0, 800, 600);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
